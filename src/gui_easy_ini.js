d039ef22600950b30da1bd6c0fa0afc2c8665947
Grovkillen <jimmy@grovkillen.com>
2019-12-31 12:15:24 +0100

    [console] Added custom console output, to differentiate console message added by the GUI

--- src/gui_easy_ini.js
+++ src/gui_easy_ini.js
@@ -4,6 +4,8 @@
 window.addEventListener("load", initiate, false);
 
 function initiate() {
+    //REDEFINE CONSOLE
+    guiEasy.newConsole();
     //PRE LAUNCH OF GUI
     for (let i = 0; i < guiEasy.startup.length; i++) {
         let x = guiEasy.startup[i].id;
@@ -23,3 +25,30 @@ function initiate() {
         guiEasy[x](x,z);
     }
 }
+
+guiEasy.newConsole = function () {
+    window.msg = (function (defaultConsole) {
+        return Object.assign({}, defaultConsole, {
+            log(text) {
+                defaultConsole.log("%cLOG: %c" + text,
+                    "background-color: #5CB85C; color: #fff; font-weight: bold; padding: 0 8px; font-size: 1.2em",
+                    "background-color: #000000; color: #5CB85C; font-weight: bold; padding-left: 8px; font-size: 1.2em");
+            },
+            info(text) {
+                defaultConsole.info("%cINFO: %c" + text,
+                    "background-color: #337AB7; color: #fff; font-weight: bold; padding: 0 8px; font-size: 1.2em",
+                    "background-color: #000000; color: #337AB7; font-weight: bold; padding-left: 8px; font-size: 1.2em");
+            },
+            warn(text) {
+                defaultConsole.warn("%cWARN: %c" + text,
+                    "background-color: #F0AD4E; color: #fff; font-weight: bold; padding: 0 8px; font-size: 1.2em",
+                    "background-color: #000000; color: #F0AD4E; font-weight: bold; padding-left: 8px; font-size: 1.2em");
+            },
+            error(text) {
+                defaultConsole.error("%cERROR: %c" + text,
+                    "background-color: #D9534F; color: #fff; font-weight: bold; padding: 0 8px; font-size: 1.2em",
+                    "background-color: #000000; color: #D9534F; font-weight: bold; padding-left: 8px; font-size: 1.2em");
+            }
+        })
+    }(window.console));
+};
